fad1059f4739152ab1ccec9539b494ba36517923
Geo12862 <74008077+Geo12862@users.noreply.github.com>
2022-06-01 22:17:07 +0200

    feat: comprobaciones con alert

--- src/js/registro.js
+++ src/js/registro.js
@@ -3,10 +3,13 @@ function registrar(){
 
     http.onreadystatechange = function(){
         if (http.readyState==4 && http.status==200){
+            alert("hola");
+            alert(http.responseText);
             document.getElementById("resultado").innerHTML=http.responseText;
         }
     }
 
-    http.open("POST","http://localhost:8080/AppRestaurantes/registrar", true);
-    http.send("edad="+document.getElementById("edad").value+"email="+document.getElementById("email").value+"&&usuario="+document.getElementById("usuario").value+"&&contraseña="+document.getElementById("contraseña").value);
+    http.open("POST","http://localhost:8080/AppRestaurantes/Registrar", true);
+    http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    http.send("edad="+document.getElementById("edad").value+"&&email="+document.getElementById("email").value+"&&usuario="+document.getElementById("usuario").value+"&&contrasenya="+document.getElementById("contrasenya").value);
 }
