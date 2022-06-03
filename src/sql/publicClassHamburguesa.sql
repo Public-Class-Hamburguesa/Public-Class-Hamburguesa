b5647a26f25d903205fa16ab1cffb5c129046f8d
ToniFeliu <91744505+ToniFeliudeC@users.noreply.github.com>
2022-06-03 21:26:36 +0200

    Triggers, Procedure and Table additions
    Ahora la tabla "producto" tiene la columna "popularidad" cuyo valor comienza en 0 y aumenta en uno cada vez que dicho producto sea escogido, almenos una vez, en una comanda.
    Se ha añadido una nueva tabla (clientesBaja) en la que se almacenarán los datos de los clientes que se den de baja, al ser eliminados de la tabla clientes.

--- src/sql/publicClassHamburguesa.sql
+++ src/sql/publicClassHamburguesa.sql
@@ -5,15 +5,14 @@ CREATE DATABASE publicClassHamburguesa;
 USE publicClassHamburguesa;
 
 CREATE TABLE empleado(
-	id VARCHAR(10),
+	id INT auto_increment,
     nombre VARCHAR(256),
     edad INT,
     PRIMARY KEY (id)
 );
     
-
 CREATE TABLE cliente(
-	id VARCHAR(10),
+	id INT auto_increment,
     usuario VARCHAR(256),
     contraseña VARCHAR(256),
     email VARCHAR(256),
@@ -21,31 +20,91 @@ CREATE TABLE cliente(
     PRIMARY KEY (id)
 );
 
+CREATE TABLE clientesBaja(
+	id INT auto_increment,
+    email VARCHAR(256),
+    edad INT,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE producto(
-	id VARCHAR(12),
+	id INT auto_increment,
     nombre VARCHAR(256),
-    precio INT,
+    precio FLOAT,
     stock INT,
+    popularidad INT,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE comanda(
-	id VARCHAR(10),
+	id INT auto_increment,
     precio FLOAT,
-    idEmpleado VARCHAR(10),
-    idCliente VARCHAR(10),
+    idEmpleado INT,
+    idCliente INT,
     PRIMARY KEY (id, idEmpleado, idCliente),
     FOREIGN KEY (idEmpleado) REFERENCES empleado(id),
     FOREIGN KEY (idCliente) REFERENCES cliente(id)
 );
 
 CREATE TABLE lineaComanda(
-	id VARCHAR(10),
+	id INT auto_increment,
     precio INT,
     cantidad INT,
-    idComanda VARCHAR(10),
-    idProducto VARCHAR(12),
+    idComanda INT,
+    idProducto INT,
     PRIMARY KEY(id, idComanda, idProducto),
     FOREIGN KEY(idComanda) REFERENCES comanda(id),
     FOREIGN KEY(idProducto) REFERENCES producto(id)
 );
+
+/*Hi ha d’haver-hi un trigger per a insercions, un per a modificacions i un per a eliminacions amb una funcionalitat definida dins l’aplicació. 
+Ha de comptar amb al manco un procediment emmagatzemat què s’empri a l’aplicació.*/
+DELIMITER //
+CREATE TRIGGER reducirStock BEFORE INSERT ON lineaComanda
+       FOR EACH ROW
+       BEGIN
+		UPDATE producto
+		SET stock = stock - NEW.cantidad
+		WHERE id = NEW.idProducto; 
+       END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER aumentarPopularidad BEFORE UPDATE ON producto
+       FOR EACH ROW
+       BEGIN
+		UPDATE producto
+		SET popularidad = popularidad + 1
+		WHERE id = OLD.id; 
+       END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER clienteBaja BEFORE DELETE ON cliente
+       FOR EACH ROW
+       BEGIN
+		INSERT INTO clientesBaja (id, email, edad)
+		VALUES (OLD.id, OLD.email, OLD.edad);
+       END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE GetAllProducts()
+BEGIN
+	SELECT * FROM producto;
+END; //
+DELIMITER ;
+
+CALL GetAllProducts();
+
+
+
+INSERT INTO producto (nombre, precio, stock, popularidad)
+VALUES ("Toni Pepperoni", 13, 100, 0);
+
+INSERT INTO producto (nombre, precio, stock, popularidad)
+VALUES ("Jorge's Wrap", 11.5, 100, 0);
+
+INSERT INTO producto (nombre, precio, stock, popularidad)
+VALUES ("Daniel Ocean", 14.5, 100, 0);
