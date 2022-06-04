de783deeafc0454c9836ce3111b3a7ac4d48ffb6
Geo12862 <74008077+Geo12862@users.noreply.github.com>
2022-06-04 23:57:26 +0200

    Create Login.java

---
+++ AppRestaurantes/src/main/java/Login.java
@@ -0,0 +1,33 @@
+import jakarta.servlet.http.HttpServlet;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//Comprobar que esta en la base de datos, sino no puede hacer login ERROR y sugerir registrarse comprar si está el listado de cliente de baja
+		// Si Login correcto, mostrar datos usuario (nombre usuario)
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
