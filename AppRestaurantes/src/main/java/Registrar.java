449764b819c3a97724a92f11573ca0e03117f958
Geo12862 <74008077+Geo12862@users.noreply.github.com>
2022-06-05 00:00:16 +0200

    Create Registrar.java

---
+++ AppRestaurantes/src/main/java/Registrar.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class hello
+ */
+@WebServlet("/Registrar")
+public class Registrar extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	// http://localhost:8080/AppRestaurantes/hello
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Servlet");
+		Cliente cliente = new Cliente(Integer.valueOf(request.getParameter("edad")), request.getParameter("email"), request.getParameter("usuario"), request.getParameter("contrasenya"));
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		if(cliente.registrarCliente()) {
+			response.getWriter().append("Usuari Resgistrat correctament");
+			System.out.println("Servlet good");
+		}else {
+			response.getWriter().append("Error: Usuari no resgistrat");
+			System.out.println("Servlet bad");
+		}
+	}
+
+}
