324b0b71ac23991e99f12a6cd648f3edc76b5cdf
Geo12862 <74008077+Geo12862@users.noreply.github.com>
2022-06-08 22:08:55 +0200

    feat: comprobar usuario existente

--- AppRestaurantes/src/main/java/Cliente.java
+++ AppRestaurantes/src/main/java/Cliente.java
@@ -24,34 +24,46 @@ public class Cliente extends Persona{
 	}
 		
 	@SuppressWarnings("finally")
-	public boolean registrarCliente() {
+	public boolean registrarCliente()  {
 		boolean  resultat = true;
-		Persona persona = new Persona();
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			String url = "jdbc:mysql://localhost:3306/publicclasshamburguesa";
-			Connection con = DriverManager.getConnection(url, "root", "Namiki1223"); //(url, usuario, contraseña)
-			Statement st = con.createStatement();
-			String query = "INSERT INTO cliente(id,edad,email,usuario,contraseña) VALUES ('1','"+persona.getEdad()+"','"+this.getEmail()+"','"+this.getUsuario()+"','"+this.getContraseña()+"')";
-			st.executeUpdate(query);  //executeQuery para cuando hacemos un select, executeUpdate para un Insert
+			if (!existirUsuario(usuario)) {
+				try {
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					String url = "jdbc:mysql://localhost:3306/publicclasshamburguesa";
+					Connection con = DriverManager.getConnection(url, "root", "Namiki1223"); //(url, usuario, contraseña)
+					Statement st = con.createStatement();
+					String query = "INSERT INTO cliente(edad,usuario,contraseña,email) VALUES ('"+super.getEdad()+"','"+this.getUsuario()+"','"+this.getContraseña()+"','"+this.getEmail()+"')";
+					System.out.println(query);
+					st.executeUpdate(query);  //executeQuery para cuando hacemos un select, executeUpdate para un Insert
+					
+				} catch (Exception e) {
+					resultat = false;
+					System.out.println(e.getMessage());
+				} finally {
+					return resultat;
+				}
+			}else {
+				resultat = false;
+			}
 		} catch (Exception e) {
-			resultat = false;
-			System.out.println(e.getMessage());
-		} finally {
-			return resultat;
-		}
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}return resultat;
+		
 		
 	}
 	
+	
 	public static String selectUsuario(String usuario) throws SQLException, ClassNotFoundException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		String url = "jdbc:mysql://localhost:3306/daw";
+		String url = "jdbc:mysql://localhost:3306/publicClassHamburguesa";
 		Connection con = DriverManager.getConnection(url, "root", "Namiki1223"); //(url, usuario, contraseña)
 		Statement st = con.createStatement();
 		
 		String query = "SELECT usuario, contraseña FROM cliente WHERE usuario='"+usuario+"'"; // ?
+		System.out.println(query);
 		ResultSet rs = st.executeQuery(query);
-		
 		String resultat = "<p>Usuario</p>";
 		
 		while (rs.next()) {
@@ -59,6 +71,29 @@ public class Cliente extends Persona{
 						+"<p>"+rs.getString("usuario")+"</p>";
 		}
 		return resultat;
+	}
+	
+	public static boolean existirUsuario(String usuario) throws SQLException, ClassNotFoundException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		String url = "jdbc:mysql://localhost:3306/publicClassHamburguesa";
+		Connection con = DriverManager.getConnection(url, "root", "Namiki1223"); //(url, usuario, contraseña)
+		Statement st = con.createStatement();
+		
+		String query = "SELECT usuario FROM cliente WHERE usuario='"+usuario+"'"; // ?
+		System.out.println(query);
+		ResultSet rs = st.executeQuery(query);
+		
+		String resultat ="";
+		while (rs.next()) {
+			resultat = rs.getString("usuario");
+			
+		}
+		System.out.print(resultat.equals(usuario));
+		if(resultat.equals(usuario)) {
+			return true;
+		}else {
+			return false;
+		}	
 	}
 
 	//Getters y Setters
